36e43d03a24dbbca2dd75a79f2fea07a7a017a76
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-07-25 16:16:00 +0530

    feat: add CLI command for btc-redeem to accept hashlock and secret

---
+++ cli/btc-redeem.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const argsSchema = z.object({
+  hashlock: z.string(),
+  secret: z.string(),
+});
+
+const args = argsSchema.parse(Object.fromEntries(process.argv.slice(2).map(arg => {
+  const [k, v] = arg.replace(/^--/, '').split('=');
+  return [k, v];
+})));
+
+console.log(JSON.stringify({
+  action: 'btc-redeem',
+  hashlock: args.hashlock,
+  secret: args.secret,
+  note: 'Stub: sign and broadcast redeem tx using this secret',
+}, null, 2)); 
